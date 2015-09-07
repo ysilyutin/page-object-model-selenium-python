0813f0b5f02ba3f0b4a779384159346722d6a040
Yegor Silyutin <ysilyutin@gmail.com>
2015-09-07 11:34:24 -0700

    [Initialization] Base structure created

---
+++ locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators(object):
+    GO_BUTTON = (By.ID, 'submit')
+
+
+class SearchResultsPageLocators(object):
+    pass
---
+++ page.py
@@ -0,0 +1,33 @@
+from element import BasePageElement
+from locators import MainPageLocators
+
+
+class SearchTextElement(BasePageElement):
+
+    locator = 'q'
+
+
+class BasePage(object):
+
+    def __init__(self, driver):
+        self.driver = driver
+
+
+class MainPage(BasePage):
+
+    search_text_element = SearchTextElement()
+
+    def is_title_matches(self):
+        return "Python" in self.driver.title
+
+    def click_go_button(self):
+        element = self.driver.find_element(*MainPageLocators.GO_BUTTON)
+        element.click()
+
+
+class SearchResultsPage(BasePage):
+
+    def is_results_found(self):
+        # Probably should search for this text in the specific page
+        # element, but as for now it works fine
+        return "No results found." not in self.driver.page_source
